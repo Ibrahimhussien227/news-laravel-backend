372e9c200ec86c41c61dff2c52d6c19e0cb95fb9
Ibrahimhussien227 <ibrahimhussien227@gmail.com>
2023-03-08 01:19:32 +0300

    prittier

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Register User
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function signup(SignupRequest $request)
+    {
+        try {
+            $data = $request->validated();
+
+            if ($data->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $data->errors()
+                ], 401);
+            }
+
+            $user = User::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "password" => bcrypt($request->password),
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'token' => $user->createToken("API_TOKEN")->plainTextToken,
+                ],
+            ], 200);
+
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Login User
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        try {
+            $credentials = $request->validated();
+
+            if ($credentials->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $credentials->errors()
+                ], 401);
+            }
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            $user->tokens()->delete();
+    
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'token' => $user->createToken("API_TOKEN")->plainTextToken,
+                ],
+            ], 200);
+
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage()
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+
+        if ( $user ) {
+            $user->tokens()->delete();
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Logged Out Successfully',
+        ], 200);
+    }    
+}
